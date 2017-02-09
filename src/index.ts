66d16ee8646a8a788ee96d2c7a970dc7e314f7ad
Dan Hiester <dan.hiester@gmail.com>
2017-02-08 22:23:34 -0800

    Eliminate unneeded semicolons.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import * as express from "express";
-import * as path from "path";
+import * as express from "express"
+import * as path from "path"
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello Typescript!');
-});
+})
 
 app.get('/:name', (req, res) => {
-  let name: string = req.params.name;
+  const name: string = req.params.name;
   res.send('Hello ' + name + '!');
-});
+})
 
 app.listen('8080');
 console.log('\nApp is running. To view it, open a web browser to http://localhost:8080.\nTo be greeted by the app, visit http://localhost:8080/YourName.\n\nQuit app with ctrl+c.');
